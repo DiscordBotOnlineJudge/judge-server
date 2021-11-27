dedf95d6a8eb0678916338ba1646882d47085872
ji-mmyliu <jimmyjcl753@gmail.com>
2021-11-26 23:31:39 -0500

    Remove data.out after verdict check

--- judging.py
+++ judging.py
@@ -139,7 +139,7 @@ def judge(problem, bat, case, compl, cmdrun, judgeNum, timelim, username, sc, se
         exitcode = getIsolate[2]
 
         # Cleanup sandbox and output files
-        os.system("isolate --cg --cleanup > /dev/null && isolate --cg --init > /dev/null && rm Judge" + str(judgeNum) + "/data.out")
+        os.system("isolate --cg --cleanup > /dev/null && isolate --cg --init > /dev/null")
 
         taken = "{x:.3f}".format(x = ft)
 
@@ -181,3 +181,4 @@ def judge(problem, bat, case, compl, cmdrun, judgeNum, timelim, username, sc, se
             f.write(str(exc_type) + " " + str(fname) + " " + str(exc_tb.tb_lineno) + "\n")
             f.flush()
             f.close()
+    os.remove("Judge" + str(judgeNum) + "/data.out")
