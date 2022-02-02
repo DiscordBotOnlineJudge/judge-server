42eeffe489dfa757268acc46e45b5af6736fcc3a
ji-mmyliu <jimmyjcl753@gmail.com>
2022-02-01 21:01:57 -0500

    Update submission.py

--- submission.py
+++ submission.py
@@ -16,6 +16,7 @@ def clean(src):
     return src.replace("`", "")
 
 def edit(settings, content, sub_id):
+    print("Updating submission status", sub_id)
     settings.update_one({"type":"submission", "id":sub_id}, {"$set":{"output":content}})
 
 def submit(storage_client, settings, username, source, lang, problem, judgeNum, attachment, sub_id) -> int:
